7ad06d69b184b4963f209d95bb68849e4bc47a9b
pitybea <wangzhp85@gmail.com>
2013-05-20 00:20:36 +0900

    update

--- tp.lisp
+++ tp.lisp
@@ -4,3 +4,22 @@
 			  1
 			  (* n (rect (- n 1))))))
 	     (rect x)))
+
+(defmacro nlet (name args &rest body)
+	   `(labels ((,name ,(mapcar #'car args)
+		       ,@body))
+	      (,name ,@(mapcar #'cadr args))))
+
+(defun nlet-fact (x)
+	   (nlet fact ((n x))
+		 (if (zerop n) 
+		     1
+		     (* n (fact (- n 1))))))
+
+(defun flattern (lst)
+	   (labels ((rect (x acc)
+		      (cond
+			((null x) acc)
+			((atom (car x)) (cons (car x) (rect (cdr x) acc)))
+			(t (rect (car x) (rect (cdr x) acc))))))
+	    (rect lst nil)))
